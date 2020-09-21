8fe9274f44f5d4c9212351c9946f0e83cb25feaf
Francesco La Regina <64254059+frankthequeen@users.noreply.github.com>
2020-09-21 12:12:50 +0200

    Update TokenNGN.sol
    New description

--- TokenNGN.sol
+++ TokenNGN.sol
@@ -17,12 +17,14 @@ import "./ERC20Burnable.sol";
  *
  *  - ability for holders to burn (destroy) their tokens
  *  - ability for holders to pledge (lock over another account) their tokens
- *  - a minter role that allows for token minting (creation)
- *  - a banker role that allows for others' tokens management (transfer, burning, allowance)
+ *  - ability of creating escrow wallets for the deposit of tokens (intended for new plant projects)
  *  - a contributor role that allows to deposit tokens on escrow wallets
- *  - a notary role that allows to finalize the transfer of pledged token and to unlock token transfers from escrow wallets
- *  - an admin role that allows to manage other roles
+ *  - ability for users to view their token balance in gross form (all tokens owned) and in net form (all available tokens, i.e. tokens owned minus those pledged to third parties)
  *  - an owner address that can manage the admin role
+ *  - an admin role that allows to manage other roles
+ *  - a notary role that allows to finalize the transfer of pledged token and to unlock token transfers from escrow wallets
+ *  - a banker role that allows for others' tokens management (transfer, burning, allowance)
+ *  - a minter role that allows for token minting (creation)
  */
 contract TokenNGN is Context, AccessControl, ERC20Burnable {
 // PLEDGE .. sender ... total amount
